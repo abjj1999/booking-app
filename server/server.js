85e057550196a53d007ba6d1d5e5f3aff0d7a309
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-08-08 18:17:57 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -41,7 +41,14 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
+const path = require("path");
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 //listner
 app.listen(8000, () => {
   connect();
